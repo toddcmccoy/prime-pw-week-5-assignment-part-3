4e892cf61c3b5c53e148c8df557c7373aefabd27
Todd McCoy <tcmccoy@gmail.com>
2022-06-03 12:55:46 -0500

    fixed addToCollection function to use an object as instructed

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,11 +3,16 @@ console.log('***** Music Collection *****')
 let collection=[];
 
 function addToCollection(title, artist, yearPublished){
-    let album = (title + artist + yearPublished);
+    const album = {
+    title: title,
+    artist: artist,
+    yearPublished: yearPublished
+    }
     collection.push(album);
     console.log(album);
     return album
 }
 
 addToCollection('Nevermind, ', 'Nirvana, ', 1993);
+addToCollection('Caught in the act, ','Styx, ', 1983);
 
