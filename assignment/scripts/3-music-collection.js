37375a1221372ca5b4124d3be2d8ed0443d7bef9
Todd McCoy <tcmccoy@gmail.com>
2022-06-03 13:59:07 -0500

    working on showCollection function.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -11,7 +11,7 @@ function addToCollection(title, artist, yearPublished){
     collection.push(album);
     console.log(album);
     return album
-}
+}// end addToCollection
 
 addToCollection('Nevermind ', 'Nirvana ', 1991);
 addToCollection('Kilroy Was Here ','Styx ', 1983);
@@ -19,7 +19,14 @@ addToCollection('Paradise Theatre ','Styx ', 1981);
 addToCollection('Moving Pictures ','Rush ', 1981);
 addToCollection('Test for Echo ','Rush ', 1996);
 addToCollection('Second Helping ','Lynyrd Skynyrd ', 1974);
+// end albums
 
 console.log(collection);
 
-
+function showCollection(){
+    for (let i = 0; i < collection.length; i++){
+        console.log(collection[i]);
+      }
+}
+console.log(collection.length);
+//showCollection;
