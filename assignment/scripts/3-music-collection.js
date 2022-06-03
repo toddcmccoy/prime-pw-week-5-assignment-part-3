c549431fde8b96ae6f0ed6d7097d570384c1f22a
Todd McCoy <tcmccoy@gmail.com>
2022-06-03 12:08:00 -0500

    created addToCollection function. first attempt.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,2 +1,13 @@
 console.log('***** Music Collection *****')
+
 let collection=[];
+
+function addToCollection(title, artist, yearPublished){
+    let album = (title + artist + yearPublished);
+    collection.push(album);
+    console.log(album);
+    return album
+}
+
+addToCollection('Nevermind, ', 'Nirvana, ', 1993);
+
